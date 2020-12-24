47bfd06e955c547d50aa966f76ad9b48ed55615c
anujohn123 <anujobin171217@gmail.com>
2020-12-24 08:24:11 +0530

    update project

--- src/main/java/com/qa/hubspot/pages/contactsPages.java
+++ src/main/java/com/qa/hubspot/pages/contactsPages.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.DataProvider;
+
 
 import com.qa.hubspot.basepage.basePage;
 import com.qa.hubspot.util.ExcelUTil;
